b040767ad8bc158a348a3134ceeb50e8856e9007
godlzr <godlzr@gmail.com>
2016-01-18 11:37:35 -0500

    config commandline

--- dotnet-clara/Program.cs
+++ dotnet-clara/Program.cs
@@ -8,6 +8,7 @@ using System.Drawing;
 using System.Net;
 using System.Net.Http;
 using System.IO;
+using System.Reflection;
 using dotnet_clara.lib;
 using CommandLine;
 using CommandLine.Text;
@@ -17,20 +18,62 @@ namespace dotnet_clara
     class Program
     {
         lib.Clara clara;
-        Config config;
-        Cli options;
+        
         public Program()
         {           
-            config = new Config();
-            clara = new lib.Clara(config);
-            options = new Cli();
+            
+            clara = new lib.Clara();
+            
         }
         
         static void Main(string[] args)
         {
-            Program p = new Program();
-            var result = Parser.Default.ParseArguments<Cli>(args);
-            var str = result;
+            string invokedVerb = null;
+            object invokedVerbInstance = null;
+
+            Config config = new Config();
+            var options = new Options();
+
+            if (CommandLine.Parser.Default.ParseArguments(args, options,
+              (verb, subOptions) =>
+              {
+                  // if parsing succeeds the verb name and correct instance
+                  // will be passed to onVerbCommand delegate (string,object)
+                  invokedVerb = verb;
+                  invokedVerbInstance = subOptions;
+              }))
+            {
+                if (invokedVerb == "set")
+                {
+                    PropertyInfo[] properties = typeof(Options.SetSubOptions).GetProperties();
+
+                    foreach (PropertyInfo property in properties)
+                    {
+                        if (property.GetValue(invokedVerbInstance) != null)
+                        {
+                            config.SetConfig(property.Name, (string)property.GetValue(invokedVerbInstance));
+                            Console.WriteLine("[INFO]: new {0}:{1}", property.Name, config.GetOneConfigInfo(property.Name));
+                        }                            
+                    }
+                }
+                if (invokedVerb == "get")
+                {
+                    PropertyInfo[] properties = typeof(Options.GetSubOptions).GetProperties();
+
+                    foreach (PropertyInfo property in properties)
+                    {
+                        if ((bool)property.GetValue(invokedVerbInstance) == true)
+                            Console.WriteLine("[INFO]:{0}:{1}", property.Name, config.GetOneConfigInfo(property.Name));
+                    }
+                }
+
+            }
+            else
+            {
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+
+
         }
 
     }
--- dotnet-clara/lib/Options.cs
+++ dotnet-clara/lib/Options.cs
@@ -10,22 +10,40 @@ namespace dotnet_clara.lib
 {
     public class Options
     {
-        [Option('r', "read", Required = true,
-          HelpText = "Input files to be processed.")]
-        public IEnumerable<string> InputFiles { get; set; }
+        public Options()
+        {
+            // Since we create this instance the parser will not overwrite it
+            SetVerb = new SetSubOptions();
+        }
 
-        // Omitting long name, default --verbose
-        [Option(
-          HelpText = "Prints all messages to standard output.")]
-        public bool Verbose { get; set; }
+        [VerbOption("set", HelpText = "Record changes to the repository.")]
+        public SetSubOptions SetVerb { get; set; }
 
-        [Option(Default = "中文",
-          HelpText = "Content language.")]
-        public string Language { get; set; }
+        [VerbOption("get", HelpText = "Update remote refs along with associated objects.")]
+        public GetSubOptions AddVerb { get; set; }
 
-        [Value(0, MetaName = "offset",
-          HelpText = "File offset.")]
-        public long? Offset { get; set; }
+
+        public class SetSubOptions
+        {
+            [Option('u',"username", HelpText = "Tell the command to automatically stage files.")]
+            public string username { get; set; }
+            // Remainder omitted
+            [Option('a',"apiToken", HelpText = "Tell the command to automatically stage files.")]
+            public string apiToken { get; set; }
+            [Option('h',"host", HelpText = "Tell the command to automatically stage files.")]
+            public string host { get; set; }
+        }
+
+        public class GetSubOptions
+        {
+            [Option('u', "username", HelpText = "Tell the command to automatically stage files.")]
+            public bool username { get; set; }
+            // Remainder omitted
+            [Option('a', "apiToken", HelpText = "Tell the command to automatically stage files.")]
+            public bool apiToken { get; set; }
+            [Option('h', "host", HelpText = "Tell the command to automatically stage files.")]
+            public bool host { get; set; }
+        }
     }
 
 }
